cd6410ee311e5deb5e637e696c5a746b1f3bed22
Daniel Alves <alvesdaniel@ua.pt>
2023-02-28 11:40:49 +0000

    style variables

---
+++ smartbreak/src/styles/variables.js
@@ -0,0 +1,71 @@
+// ---- MAIN COLORS PALETE ----
+const mainBlue = "#07407B";
+const lightBlue = "#7591AE";
+const thirdBlue = "#B5C6D7";
+const mainOrange = "#F57738";
+const lightOrange = "#EBAC8D";
+const thirdOrange = "#FCD6C3";
+const lightBackgroundColor = "#FEFEFE";
+const darkerColor = "#333333";
+const greyColor = "#555555";
+
+// ---- GOALS COLORS ----
+const lowPriorityColor = "#07407B";
+const mediumPriorityColor = "#F57738";
+const highPriorityColor = "#F55038";
+
+// ---- FONT SIZES ----
+const dashboardChargeSize = 40; //gotham medium 40
+const dashboardGoalsSize = 32; //gotham medium 32
+const pageTitleSize = 24; //gotham medium 24
+const pageSubtitleSize = 18; //gotham medium 18
+const pageTextSize = 16; //gotham book 16
+const pageSmallTextSize = 14; //gotham book 14
+
+// ---- TABBAR ELEMENTS ----
+const navbarTextSize = 12; //gotham book 12
+const selectedColor = "#07407B"; //icon selected
+const unselectedColor = "#333333"; //icon unselected
+
+// ---- ICONS ELEMENTS ----
+const iconWidth = 20;
+const iconColor = "#333333";
+const metricsIconContainerIndividualColor = "#B5C6D7";
+const metricsIconContainerTeamColor = "#FCD6C3";
+const rewardIconContainerColor = "#FCD6C3";
+
+// ---- TOGGLES -----
+// -- Dashboard --
+const selectedToggleBackground = "#FFFFFF";
+const selectedToggleTextColorIndividual = "#07407B";
+const selectedToggleTextColorDepartamental = "#F57738";
+const unselectedToggleBackgroundColorIndividual = "#B5C6D7";
+const unselectedToggleBackgroundColorDepartamental = "#FCD6C3";
+const unselectedToggleTextColor = "#333333";
+
+// -- Switch --
+const switchOnColor = "#07407B";
+const switchOffColor = "#8E8E8E";
+const switchIndicatorColor = "#FFFFFF";
+
+// ---- BUTTONS ELEMENTS ----
+const primaryButtonBackground = "#07407B";
+const primaryButtonBorderColor = "#07407B";
+const primaryButtonTextColor = "#FFF";
+const secondaryButtonBackground = "transparent";
+const secondaryButtonBorderColor = "#F57738";
+const secondaryButtonTextColor = "#F57738";
+const normalButtonSize = 16;
+const smallButtonSize = 14;
+
+// ---- BACKGROUND PADDING -----
+const backgroundPaddingTop = 65;
+const backgroundPaddingLateral = 25;
+
+// ---- TEXT PADDING ----
+const textPadding = 15;
+const iconPadding = 15;
+
+// ---- BORDER RADIUS ----
+const normalButtonRadius = 15;
+const smallButtonRadius = 8;
