cc8f4d4a34e3a16c7ff008c44c167bd41e886ecc
Diana Siso <diana.siso@ua.pt>
2023-05-02 22:08:54 +0100

    .

--- smartbreak/src/screens/profile/accessibility.js
+++ smartbreak/src/screens/profile/accessibility.js
@@ -1,17 +1,4 @@
 import { StatusBar } from "expo-status-bar";
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  ScrollView,
-  View,
-  ToastAndroid,
-  Text,
-  Switch,
-  Dimensions,
-} from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-=======
 import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
@@ -24,7 +11,6 @@ import {
 } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
->>>>>>> bab837270f44d964803e5cf2f853cd589265d21f
 import { useSelector } from "react-redux";
 
 // Firebase
@@ -38,33 +24,12 @@ import { styles } from "./../../styles/css.js";
 // Variables
 import * as CONST from "./../../styles/variables.js";
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> bab837270f44d964803e5cf2f853cd589265d21f
 export default function Accessibility({ navigation }) {
   // Loading Gotham font
   const [loaded] = useFonts({
     GothamMedium: "./../fonts/GothamMedium.ttf",
     GothamBook: "./../fonts/GothamBook.ttf",
   });
-<<<<<<< HEAD
-  const userData = useSelector((state) => state.user.userID);
-  const uid = userData;
-  const [shareData, setShareData] = useState();
-  
-  useEffect(() => {
-    firebase.firestore()
-    .collection("users_data")
-    .doc(uid)
-    .get()
-    .then((doc) => {
-        setShareData(doc.data().shareData);
-    })
-
-  },[])
-=======
 
   const [notificationsArray, setNotificationsArray] = useState([]);
   const [, updateState] = useState();
@@ -83,7 +48,6 @@ export default function Accessibility({ navigation }) {
         // console.log("Devices:", devices)
       });
   }, [userData]);
->>>>>>> bab837270f44d964803e5cf2f853cd589265d21f
 
   return (
     <SafeAreaProvider
@@ -91,56 +55,6 @@ export default function Accessibility({ navigation }) {
       style={styles.containerLight}
     >
       <ScrollView>
-<<<<<<< HEAD
-        <StatusBar style="dark" />
-        <Text 
-          accessible={true}
-          accessibilityLabel="Texto na cor preta num fundo branco escrito Acessibilidade. É o título da página."
-          style={styles.titleText}>Acessibilidade{"\n"}</Text>
-          <Text 
-            accessible={true}
-            accessibilityLabel="O utilizador, ao aceitar os Termos e Política de Privacidade,
-            autoriza expressamente a aplicação a coletar, usar, armazenar,
-            tratar, ceder ou utilizar as informações derivadas do uso da
-            aplicação, incluindo todas as informações preenchidas pelo
-            utilizador no momento em que realizar ou atualizar o seu cadastro,
-            além de outras expressamente descritas na Política de Privacidade
-            que deverá ser autorizada pelo utilizador."
-            
-            style={styles.normalText}>
-            O utilizador, ao aceitar os Termos e Política de Privacidade,
-            autoriza expressamente a aplicação a coletar, usar, armazenar,
-            tratar, ceder ou utilizar as informações derivadas do uso da
-            aplicação, incluindo todas as informações preenchidas pelo
-            utilizador no momento em que realizar ou atualizar o seu cadastro,
-            além de outras expressamente descritas na Política de Privacidade
-            que deverá ser autorizada pelo utilizador.
-            {"\n"}
-            {"\n"}
-          </Text>
-        
-          <View style={[styles.boxOptions, {borderBottomWidth: 0, borderTopWidth: 1, borderTopColor: CONST.dividerColor}]}>
-            <Text 
-              accessible={true}
-              accessibilityLabel="Texto na cor preta num fundo branco escrito Partilha de dados. Possui um switch à frente para ativar ou desativar a opção."
-              style={styles.normalText}>Partilha de dados</Text>
-            <Switch
-              style={{ marginLeft: "auto", marginRight: CONST.iconPadding }}
-              trackColor={{ false: CONST.switchOffColor, true: CONST.switchOnColor }}
-              thumbColor={shareData ? CONST.switchIndicatorColor : CONST.mainBlue}
-              value={shareData}
-              onValueChange={(() => {
-                firebase.firestore().collection('users_data').doc(uid).update({
-                  shareData : !shareData
-                })
-                setShareData(!shareData)
-                ToastAndroid.show('Alterações efetuadas com sucesso!', ToastAndroid.SHORT);
-              })}
-            />
-          </View>
-            
-          
-=======
         <StatusBar style="auto" />
         <Text style={styles.titleText}>Acessibilidade {"\n"}</Text>
         <View style={styles.boxOptions}>
@@ -320,58 +234,8 @@ export default function Accessibility({ navigation }) {
             }}
           />
         </View>
->>>>>>> bab837270f44d964803e5cf2f853cd589265d21f
       </ScrollView>
     </SafeAreaProvider>
   );
 }
 
-<<<<<<< HEAD
-=======
-// const screenWidth = Dimensions.get('window').width;
-// const screenHeight = Dimensions.get('window').height;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     paddingLeft: 25,
-//     paddingRight: 25,
-//     paddingBottom: 90,
-//   },
-
-//   options: {
-//     flex: 1,
-//     marginTop: 20,
-//     marginBottom: 10,
-//     borderRadius: 15,
-//     paddingTop: 15,
-//     paddingBottom: 15,
-//     paddingLeft: 25,
-//     width: screenWidth - 50,
-//     flexDirection: "row",
-//     alignItems: "center",
-//     textAlign: 'left',
-//     backgroundColor: "#E3ECF7",
-//   },
-//   rewards: {
-//     flexDirection: "row",
-//     alignItems: "center",
-//     justifyContent: "space-between",
-//     marginTop: 30,
-//   },
-
-//   title: {
-//     fontFamily: "GothamMedium",
-//     fontSize: 24,
-//     marginTop: 30,
-//   },
-
-//   text: {
-//     fontFamily: "GothamBook",
-//     fontSize: 16,
-//     marginLeft: 15,
-//     lineHeight: 24,
-//   },
-// });
->>>>>>> bab837270f44d964803e5cf2f853cd589265d21f
