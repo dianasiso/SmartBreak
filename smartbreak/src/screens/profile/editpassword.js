820ec92053ca65c2ff630d0656ba85e6f60a1473
Daniel Alves <alvesdaniel@ua.pt>
2023-06-19 19:06:21 +0100

    edit password com redux

--- smartbreak/src/screens/profile/editpassword.js
+++ smartbreak/src/screens/profile/editpassword.js
@@ -1,17 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { Alert } from "react-native";
-import {
-  ScrollView,
-  View,
-  Text,
-  TextInput,
-  Pressable,
-} from "react-native";
+import { ScrollView, View, Text, TextInput, Pressable } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { useSelector } from "react-redux";
 import { Eye, EyeSlash } from "iconsax-react-native";
-
 
 // CSS
 import { styles } from "./../../styles/css.js";
@@ -23,16 +15,15 @@ import * as CONST from "./../../styles/variables.js";
 // Font Gotham
 import { useFonts } from "expo-font";
 
-import { useDispatch } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { updatePassword } from "../../redux/user.js";
 
 export default function EditPassword({ navigation }) {
-
+  // ---- userData information ----
   const dispatch = useDispatch();
-
-  // ---- userData information
   const userData = useSelector((state) => state.user);
-  const dark_mode = userData.accessibility[1]
+
+  const dark_mode = userData.accessibility[1];
 
   const uid = userData.userID;
   const password = userData.password;
@@ -46,16 +37,12 @@ export default function EditPassword({ navigation }) {
   const [passwordStored, setPasswordStored] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
   const [newPassword, setNewPassword] = useState();
-  
+
   const [showPassword, setShowPassword] = useState(false);
   const [showNewPassword, setShowNewPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-
-  useEffect(() => {
-    
-  }, [])
-
+  useEffect(() => {}, []);
 
   const validate_password = (pass) => {
     if (pass.length < 8) {
@@ -80,7 +67,7 @@ export default function EditPassword({ navigation }) {
     }
   };
 
-  const changePassword = () => {
+  const changePassword = async () => {
     Alert.alert("Atenção", "Deseja confirmar as alterações?", [
       { text: "Cancelar" },
       {
@@ -88,28 +75,38 @@ export default function EditPassword({ navigation }) {
         onPress: async () => {
           if (validate()) {
             try {
-              const response = await fetch("https://sb-api.herokuapp.com/auth/password", {
-                method: "POST",
-                headers: {
-                  Authorization: "Bearer " + userData.token,
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  password : passwordStored,
-                  new_password : newPassword
-                }),
-              });
+              const response = await fetch(
+                "https://sb-api.herokuapp.com/auth/password",
+                {
+                  method: "POST",
+                  headers: {
+                    Authorization: "Bearer " + userData.token,
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    password: passwordStored,
+                    new_password: newPassword,
+                  }),
+                }
+              );
+
               if (response.ok) {
-                // ! DANIEL ATUALIZA O REDUX AQUI PFV
+                const updatedPassword = { password: newPassword };
+                dispatch(updatePassword(updatedPassword));
                 Alert.alert("Sucesso!", "Palavra-passe alterada com sucesso.");
-                navigation.navigate("ProfileSettings");
+                navigation.navigate("ProfileSettings", {
+                  reload: true,
+                });
               } else {
                 const errorData = await response.json();
                 Alert.alert("Erro!", errorData.message);
               }
             } catch (error) {
               console.error(error);
-              Alert.alert("Erro!", "Ocorreu um erro durante a mudança de estado.");
+              Alert.alert(
+                "Erro!",
+                "Ocorreu um erro durante a mudança de estado."
+              );
             }
           }
         },
@@ -120,108 +117,143 @@ export default function EditPassword({ navigation }) {
   return (
     <SafeAreaProvider
       showsVerticalScrollIndicator={false}
-      style={[dark_mode ? dark_styles.containerLight : styles.containerLight, {paddingTop: CONST.backgroundPaddingTop/2}]}
+      style={[
+        dark_mode ? dark_styles.containerLight : styles.containerLight,
+        { paddingTop: CONST.backgroundPaddingTop / 2 },
+      ]}
     >
       <ScrollView>
-        <StatusBar style={dark_mode ? "light" : "dark" } />
-        <Text 
+        <StatusBar style={dark_mode ? "light" : "dark"} />
+        <Text
           accessible={true}
           accessibilityLabel="Textoescrito Alterar Palavra-passe. É o título da página."
-          style={dark_mode ? dark_styles.titleText : styles.titleText}>Alterar palavra-passe{"\n"}</Text>
+          style={dark_mode ? dark_styles.titleText : styles.titleText}
+        >
+          Alterar palavra-passe{"\n"}
+        </Text>
         <Text
           accessible={true}
           accessibilityLabel="Texto escrito Palavra-passe atual."
-          style={dark_mode ? dark_styles.inputLabel : styles.inputLabel}>{"\n"}Palavra-passe atual</Text>
-          
-          <View style={{ flexDirection: "row", width: "100%" }}>
-              <TextInput
-                accessible={true}
-                accessibilityLabel="Campo para introdução da palavra-passe."
-                secureTextEntry={showPassword ? false : true}
-                style={[dark_mode ? dark_styles.inputField : styles.inputField, { width: "90%" }]}
-                onChangeText={(text) => setPasswordStored(text)}
-              />
-              {showPassword ? (
-                <EyeSlash
-                  style={{ marginLeft: "auto", marginRight: "auto" }}
-                  size={CONST.pageSubtitleSize}
-                  color={dark_mode ? CONST.whiteText : CONST.darkerColor}
-                  onPress={() => setShowPassword(!showPassword)}
-                />
-              ) : (
-                <Eye
-                  style={{ marginLeft: "auto", marginRight: "auto" }}
-                  size={CONST.pageSubtitleSize}
-                  color={dark_mode ? CONST.whiteText : CONST.darkerColor}
-                  onPress={() => setShowPassword(!showPassword)}
-                />
-              )}
-            </View>  
+          style={dark_mode ? dark_styles.inputLabel : styles.inputLabel}
+        >
+          {"\n"}Palavra-passe atual
+        </Text>
+
+        <View style={{ flexDirection: "row", width: "100%" }}>
+          <TextInput
+            accessible={true}
+            accessibilityLabel="Campo para introdução da palavra-passe."
+            secureTextEntry={showPassword ? false : true}
+            style={[
+              dark_mode ? dark_styles.inputField : styles.inputField,
+              { width: "90%" },
+            ]}
+            onChangeText={(text) => setPasswordStored(text)}
+          />
+          {showPassword ? (
+            <EyeSlash
+              style={{ marginLeft: "auto", marginRight: "auto" }}
+              size={CONST.pageSubtitleSize}
+              color={dark_mode ? CONST.whiteText : CONST.darkerColor}
+              onPress={() => setShowPassword(!showPassword)}
+            />
+          ) : (
+            <Eye
+              style={{ marginLeft: "auto", marginRight: "auto" }}
+              size={CONST.pageSubtitleSize}
+              color={dark_mode ? CONST.whiteText : CONST.darkerColor}
+              onPress={() => setShowPassword(!showPassword)}
+            />
+          )}
+        </View>
 
         <Text
           accessible={true}
           accessibilityLabel="Texto escrito Nova palavra-passe."
-          style={dark_mode ? dark_styles.inputLabel : styles.inputLabel}>{"\n"}Nova palavra-passe</Text>
+          style={dark_mode ? dark_styles.inputLabel : styles.inputLabel}
+        >
+          {"\n"}Nova palavra-passe
+        </Text>
         <View style={{ flexDirection: "row", width: "100%" }}>
-              <TextInput
-                accessible={true}
-                accessibilityLabel="Campo para introdução da nova palavra-passe."
-                secureTextEntry={showNewPassword ? false : true}
-                style={[dark_mode ? dark_styles.inputField : styles.inputField, { width: "90%" }]}
-                onChangeText={(text) => setNewPassword(text)}
-              />
-              {showNewPassword ? (
-                <EyeSlash
-                  style={{ marginLeft: "auto", marginRight: "auto" }}
-                  size={CONST.pageSubtitleSize}
-                  color={dark_mode ? dark_styles.whiteText : CONST.darkerColor}
-                  onPress={() => setShowNewPassword(!showNewPassword)}
-                />
-              ) : (
-                <Eye
-                  style={{ marginLeft: "auto", marginRight: "auto" }}
-                  size={CONST.pageSubtitleSize}
-                  color={dark_mode ? CONST.whiteText : CONST.darkerColor}
-                  onPress={() => setShowNewPassword(!showNewPassword)}
-                />
-              )}
-            </View>  
+          <TextInput
+            accessible={true}
+            accessibilityLabel="Campo para introdução da nova palavra-passe."
+            secureTextEntry={showNewPassword ? false : true}
+            style={[
+              dark_mode ? dark_styles.inputField : styles.inputField,
+              { width: "90%" },
+            ]}
+            onChangeText={(text) => setNewPassword(text)}
+          />
+          {showNewPassword ? (
+            <EyeSlash
+              style={{ marginLeft: "auto", marginRight: "auto" }}
+              size={CONST.pageSubtitleSize}
+              color={dark_mode ? dark_styles.whiteText : CONST.darkerColor}
+              onPress={() => setShowNewPassword(!showNewPassword)}
+            />
+          ) : (
+            <Eye
+              style={{ marginLeft: "auto", marginRight: "auto" }}
+              size={CONST.pageSubtitleSize}
+              color={dark_mode ? CONST.whiteText : CONST.darkerColor}
+              onPress={() => setShowNewPassword(!showNewPassword)}
+            />
+          )}
+        </View>
 
         <Text
           accessible={true}
           accessibilityLabel="Texto escrito Confirmar nova palavra-passe."
-          style={dark_mode ? dark_styles.inputLabel : styles.inputLabel}>{"\n"}Confirmar nova palavra-passe</Text>
+          style={dark_mode ? dark_styles.inputLabel : styles.inputLabel}
+        >
+          {"\n"}Confirmar nova palavra-passe
+        </Text>
         <View style={{ flexDirection: "row", width: "100%" }}>
-              <TextInput
-                accessible={true}
-                accessibilityLabel="Campo para introdução da confirmação da nova palavra-passe."
-                secureTextEntry={showConfirmPassword ? false : true}
-                style={[dark_mode ? dark_styles.inputField : styles.inputField, { width: "90%" }]}
-                onChangeText={(text) => setConfirmPassword(text)}
-              />
-              {showConfirmPassword ? (
-                <EyeSlash
-                  style={{ marginLeft: "auto", marginRight: "auto" }}
-                  size={CONST.pageSubtitleSize}
-                  color={dark_mode ? CONST.whiteText : CONST.darkerColor}
-                  onPress={() => setShowConfirmPassword(!showConfirmPassword)}
-                />
-              ) : (
-                <Eye
-                  style={{ marginLeft: "auto", marginRight: "auto" }}
-                  size={CONST.pageSubtitleSize}
-                  color={dark_mode ? CONST.whiteText : CONST.darkerColor}
-                  onPress={() => setShowConfirmPassword(!showConfirmPassword)}
-                />
-              )}
-            </View>  
+          <TextInput
+            accessible={true}
+            accessibilityLabel="Campo para introdução da confirmação da nova palavra-passe."
+            secureTextEntry={showConfirmPassword ? false : true}
+            style={[
+              dark_mode ? dark_styles.inputField : styles.inputField,
+              { width: "90%" },
+            ]}
+            onChangeText={(text) => setConfirmPassword(text)}
+          />
+          {showConfirmPassword ? (
+            <EyeSlash
+              style={{ marginLeft: "auto", marginRight: "auto" }}
+              size={CONST.pageSubtitleSize}
+              color={dark_mode ? CONST.whiteText : CONST.darkerColor}
+              onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+            />
+          ) : (
+            <Eye
+              style={{ marginLeft: "auto", marginRight: "auto" }}
+              size={CONST.pageSubtitleSize}
+              color={dark_mode ? CONST.whiteText : CONST.darkerColor}
+              onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+            />
+          )}
+        </View>
 
         <View>
-          <Pressable 
+          <Pressable
             accessible={true}
             accessibilityLabel="Botão da cor azul escura num fundo branco com o objetivo de guardar as alterações. Tem escrito na cor branca a palavra Concluído."
-            onPress={() => changePassword()} style={[dark_mode ? dark_styles.primaryButton : styles.primaryButton, {marginTop: CONST.backgroundPaddingLateral}]}>
-            <Text style={dark_mode ? dark_styles.primaryButtonText : styles.primaryButtonText}>
+            onPress={() => changePassword()}
+            style={[
+              dark_mode ? dark_styles.primaryButton : styles.primaryButton,
+              { marginTop: CONST.backgroundPaddingLateral },
+            ]}
+          >
+            <Text
+              style={
+                dark_mode
+                  ? dark_styles.primaryButtonText
+                  : styles.primaryButtonText
+              }
+            >
               Concluído
             </Text>
           </Pressable>
@@ -229,4 +261,4 @@ export default function EditPassword({ navigation }) {
       </ScrollView>
     </SafeAreaProvider>
   );
-} 
+}
--- smartbreak/src/screens/profile/profilesettings.js
+++ smartbreak/src/screens/profile/profilesettings.js
@@ -20,6 +20,8 @@ import {
   Autobrightness,
 } from "iconsax-react-native";
 
+import { useRoute } from "@react-navigation/native";
+
 // Font Gotham
 import { useFonts } from "expo-font";
 
@@ -37,7 +39,7 @@ import { dark_styles } from "../../styles/darkcss.js";
 // Variables
 import * as CONST from "./../../styles/variables.js";
 
-export default function ProfileSettings({ navigation }) {
+export default function ProfileSettings({ route, navigation }) {
   const dispatch = useDispatch();
   // Loading Gotham font
   const [loaded] = useFonts({
@@ -45,10 +47,26 @@ export default function ProfileSettings({ navigation }) {
     GothamBook: "./../fonts/GothamBook.ttf",
   });
 
-  
   const userData = useSelector((state) => state.user);
-  const dark_mode = userData.accessibility[1]
+  const password = userData.password;
+  const dark_mode = userData.accessibility[1];
   const uid = userData.userID;
+  const props = route.params;
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    if (route.params && route.params.reload) {
+      if (props !== undefined) {
+        setReload(true);
+      }
+
+      if (reload) {
+        setReload(false);
+      }
+    }
+  }, [route.params]);
+
+  console.log("Password stored in Redux:", password);
 
   const apagarconta = () => {
     Alert.alert("Atenção", "Deseja apagar a sua conta permanentemente?", [
@@ -98,86 +116,170 @@ export default function ProfileSettings({ navigation }) {
   return (
     <SafeAreaProvider
       showsVerticalScrollIndicator={false}
-      style={[dark_mode ? dark_styles.containerLight : styles.containerLight, { paddingTop: CONST.backgroundPaddingTop / 2 }]}
+      style={[
+        dark_mode ? dark_styles.containerLight : styles.containerLight,
+        { paddingTop: CONST.backgroundPaddingTop / 2 },
+      ]}
     >
       <ScrollView>
-      <StatusBar style={dark_mode ? "light" : "dark" } />
-        <Text 
+        <StatusBar style={dark_mode ? "light" : "dark"} />
+        <Text
           accessible={true}
           accessibilityLabel="Texto na cor preta num fundo branco escrito Definições. É o título da página."
-          style={dark_mode ? dark_styles.titleText : styles.titleText}>Definições{"\n"}</Text>
+          style={dark_mode ? dark_styles.titleText : styles.titleText}
+        >
+          Definições{"\n"}
+        </Text>
 
         <Pressable
           accessible={true}
           accessibilityLabel="Botão transparente com texto na preta num fundo branco escrito Alterar palavra-passe. É acompanhado por um icon de cadeado trancado."
-          style={[dark_mode ? dark_styles.boxOptions : styles.boxOptions, {paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding}]}
+          style={[
+            dark_mode ? dark_styles.boxOptions : styles.boxOptions,
+            { paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding },
+          ]}
           onPress={() => navigation.navigate("EditPassword")}
         >
-          <Lock1 variant="Bold" style={dark_mode ? dark_styles.boxIcon : styles.boxIcon} />
-          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}> Alterar palavra-passe</Text>
+          <Lock1
+            variant="Bold"
+            style={dark_mode ? dark_styles.boxIcon : styles.boxIcon}
+          />
+          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}>
+            {" "}
+            Alterar palavra-passe
+          </Text>
         </Pressable>
         <Pressable
           accessible={true}
           accessibilityLabel="Botão transparente com texto na preta num fundo branco escrito Notificações. É acompanhado por um icon de sino."
-          style={[dark_mode ? dark_styles.boxOptions :styles.boxOptions, {paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding}]}
+          style={[
+            dark_mode ? dark_styles.boxOptions : styles.boxOptions,
+            { paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding },
+          ]}
           onPress={() => navigation.navigate("NotificationsProfile")}
         >
-          <Notification variant="Bold" style={dark_mode ? dark_styles.boxIcon : styles.boxIcon} />
-          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}> Notificações</Text>
+          <Notification
+            variant="Bold"
+            style={dark_mode ? dark_styles.boxIcon : styles.boxIcon}
+          />
+          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}>
+            {" "}
+            Notificações
+          </Text>
         </Pressable>
         <Pressable
           accessible={true}
           accessibilityLabel="Botão transparente com texto na preta num fundo branco escrito Segurança. É acompanhado por um icon de escudo com uma pessoa estampada."
-         style={[dark_mode ? dark_styles.boxOptions : styles.boxOptions, {paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding}]}
-          onPress={() => navigation.navigate("SecurityProfile")}>
-          <SecurityUser variant="Bold" style={dark_mode ? dark_styles.boxIcon : styles.boxIcon} />
-          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}> Segurança</Text>
+          style={[
+            dark_mode ? dark_styles.boxOptions : styles.boxOptions,
+            { paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding },
+          ]}
+          onPress={() => navigation.navigate("SecurityProfile")}
+        >
+          <SecurityUser
+            variant="Bold"
+            style={dark_mode ? dark_styles.boxIcon : styles.boxIcon}
+          />
+          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}>
+            {" "}
+            Segurança
+          </Text>
         </Pressable>
         <Pressable
           accessible={true}
           accessibilityLabel="Botão transparente com texto na preta num fundo branco escrito Acessibilidade. É acompanhado por um icon da letra A."
-         style={[dark_mode ? dark_styles.boxOptions : styles.boxOptions, {paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding}]}
-          onPress={() => navigation.navigate("Accessibility")}>
-          <Autobrightness variant="Bold" style={dark_mode ? dark_styles.boxIcon : styles.boxIcon} />
-          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}> Acessibilidade</Text>
+          style={[
+            dark_mode ? dark_styles.boxOptions : styles.boxOptions,
+            { paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding },
+          ]}
+          onPress={() => navigation.navigate("Accessibility")}
+        >
+          <Autobrightness
+            variant="Bold"
+            style={dark_mode ? dark_styles.boxIcon : styles.boxIcon}
+          />
+          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}>
+            {" "}
+            Acessibilidade
+          </Text>
         </Pressable>
         <Pressable
           accessible={true}
           accessibilityLabel="Botão transparente com texto na preta num fundo branco escrito Termos de utilização. É acompanhado por um icon de folha de bloco de notas."
-          style={[dark_mode ? dark_styles.boxOptions : styles.boxOptions, {paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding}]}
-          onPress={() => navigation.navigate("TermsofUseProfile")}>
-          <DocumentText1 variant="Bold" style={dark_mode ? dark_styles.boxIcon : styles.boxIcon} />
-          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}> Termos de utilização</Text>
+          style={[
+            dark_mode ? dark_styles.boxOptions : styles.boxOptions,
+            { paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding },
+          ]}
+          onPress={() => navigation.navigate("TermsofUseProfile")}
+        >
+          <DocumentText1
+            variant="Bold"
+            style={dark_mode ? dark_styles.boxIcon : styles.boxIcon}
+          />
+          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}>
+            {" "}
+            Termos de utilização
+          </Text>
         </Pressable>
         <Pressable
           accessible={true}
           accessibilityLabel="Botão transparente com texto na preta num fundo branco escrito Centro de Ajuda. É acompanhado por um icon de ponto de interrogação."
-          style={[dark_mode ? dark_styles.boxOptions : styles.boxOptions, {paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding}]}
-          onPress={() => navigation.navigate("HelpCenterProfile")}>
-          <MessageQuestion variant="Bold" style={dark_mode ? dark_styles.boxIcon : styles.boxIcon} />
-          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}> Centro de ajuda</Text>
+          style={[
+            dark_mode ? dark_styles.boxOptions : styles.boxOptions,
+            { paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding },
+          ]}
+          onPress={() => navigation.navigate("HelpCenterProfile")}
+        >
+          <MessageQuestion
+            variant="Bold"
+            style={dark_mode ? dark_styles.boxIcon : styles.boxIcon}
+          />
+          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}>
+            {" "}
+            Centro de ajuda
+          </Text>
         </Pressable>
-        <Pressable 
+        <Pressable
           accessible={true}
           accessibilityLabel="Botão transparente com texto na preta num fundo branco escrito Aapgar conta. É acompanhado por um icon de caixote de lixo."
-          style={[dark_mode ? dark_styles.boxOptions : styles.boxOptions, {paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding}]}
-          onPress={apagarconta}>
+          style={[
+            dark_mode ? dark_styles.boxOptions : styles.boxOptions,
+            { paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding },
+          ]}
+          onPress={apagarconta}
+        >
           <Trash
             variant="Bold"
             style={dark_mode ? dark_styles.boxIcon : styles.boxIcon}
-            onPress={apagarconta}/>
-          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}> Apagar conta</Text>
+            onPress={apagarconta}
+          />
+          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}>
+            {" "}
+            Apagar conta
+          </Text>
         </Pressable>
-        <Pressable 
+        <Pressable
           accessible={true}
           accessibilityLabel="Botão transparente com texto na preta num fundo branco escrito Terminar sessão. É acompanhado por um icon de porta com indicação de saída."
-          style={[dark_mode ? dark_styles.boxOptions : styles.boxOptions, {paddingTop: CONST.textPadding, paddingBottom: CONST.textPadding, borderBottomWidth: 0}]}
-          onPress={terminarsessao}>
+          style={[
+            dark_mode ? dark_styles.boxOptions : styles.boxOptions,
+            {
+              paddingTop: CONST.textPadding,
+              paddingBottom: CONST.textPadding,
+              borderBottomWidth: 0,
+            },
+          ]}
+          onPress={terminarsessao}
+        >
           <Logout
             variant="Bold"
             style={dark_mode ? dark_styles.boxIcon : styles.boxIcon}
-            onPress={terminarsessao}/>
-          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}> Terminar sessão</Text>
+            onPress={terminarsessao}
+          />
+          <Text style={dark_mode ? dark_styles.normalText : styles.normalText}>
+            {" "}
+            Terminar sessão
+          </Text>
         </Pressable>
       </ScrollView>
     </SafeAreaProvider>
